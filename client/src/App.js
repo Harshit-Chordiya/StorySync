d3102d00375239c09df40bc67b4e2c80147f857a
Harshit-Chordiya <harshitchordiya03@gmail.com>
2024-01-16 23:57:54 +0530

    created post and header component

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './Header';
+import Post from './Post';
+
+function App() {
+  return (
+   <main>
+    <Header/>
+     <Post/>
+     <Post/>
+     <Post/>
+     <Post/>
+   </main>
+  );
+}
+
+export default App;
---
+++ client/src/Post.js
@@ -0,0 +1,18 @@
+export default function Post() {
+    return(
+
+        <div className="post">
+        <div className="image">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUKznNzmjudNM3KDlySwks6vX8hyC-jfdjpMrnjD2y4g&s" alt=""/>
+        </div>
+        <div className="text">
+        <div className="heading">TRIAL STORY TRIAL STORY TRIAL STORY</div>
+        <p className="info">
+          <a className="author">HARSHIT CHORDIYA </a>
+          <time>2023-01-06 16:45</time>
+        </p>
+        <p className="summary">this is  a paragraph this is  a paragraphthis is  a paragraphthis is a paragraphthis is a paragraph</p>
+        </div> </div>
+    );
+    
+};
